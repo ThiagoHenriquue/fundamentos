1de6de00719397be0e2cafe2273a50bb4b4cf15a
ThiagoHenriquue <th14henriquue@gmail.com>
2026-09-01 21:37:43 -0300

    aprendendo e entendendo listas

---
+++ 9-lista.py
@@ -0,0 +1,14 @@
+filmMatrix = ["Matrix", 1999, 8.7, True ]
+print((filmMatrix))
+
+filmsList = ["Inception", "The Shawshanks Redemption",
+             "The Drak knight", "Pulp Fiction", "Interistelar"]
+
+# 1- Buscar os dois primeiros itens da lista 
+print(filmsList[:2])
+# 2 - Buscar o ultimo intem da lista 
+print(filmsList[-1])
+# 3 - Buscar filmes ate uma determinada posicao 
+print(filmsList[:3])
+# 4 - Buscar filmes de uma posicao em diante 
+print(filmsList[1:4])
