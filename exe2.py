d6639c3259fa34f722132306c13b7772f296da75
ThiagoHenriquue <th14henriquue@gmail.com>
2026-09-01 21:58:55 -0300

    exicitando sobre as listas

---
+++ exe2.py
@@ -0,0 +1,10 @@
+
+numero = [3, 4, 5]
+#lista completa
+print(numero)
+# O primeiro elemento da lista
+print(numero[:1])
+# A soma de todos os elementos da lista
+total = sum(numero)
+print(total)
+
