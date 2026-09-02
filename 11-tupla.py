57f96124f1e36f78b2182252ef4f02b504a11f11
ThiagoHenriquue <th14henriquue@gmail.com>
2026-09-01 22:49:02 -0300

    Update 11-tupla.py

--- 11-tupla.py
+++ 11-tupla.py
@@ -8,10 +8,10 @@ print(filmsTuple[:2])
 # 2 - Buscar o ultimo item da tupla
 print(filmsTuple[-1])
 
-# 3 - Buscar determinada posicao 
+# 3 - Buscar determinada posicao
 print(filmsTuple[:3])
 
-# 4 - Buscar filmes de uma posicao em diante 
+# 4 - Buscar filmes de uma posicao em diante
 print(filmsTuple[3:])
 
 # 5 - Recuperar um item da tupla pelo nome 
